55ac7f79c900d9901d397186ac9c18fd37718f0f
JezzaR <jeremyruss43@gmail.com>
2020-04-30 23:03:12 +0100

    Added all 20 levels in

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ status = cycle(["Ready for action!",f"Create a character with {prefix} create!",
 client.remove_command("help")
 currentFolder = os.path.dirname(os.path.realpath(__file__))
 # RPG Vars
-xpRequired = [0,300,900,2700,]
+xpRequired = [0,300,900,2700,6500,14000,23000,34000,48000,64000,85000,100000,120000,140000,165000,195000,225000,265000,305000,355000]
 helpPage = 1
 helpInit = ""
 helpMsg = ""
